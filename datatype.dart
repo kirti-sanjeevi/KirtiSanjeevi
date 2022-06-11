f23420dee1c22ae6ef84a5141772fa01ed874507
kirti-sanjeevi <98817523+kirti-sanjeevi@users.noreply.github.com>
2022-06-11 18:42:18 +0530

    Add files via upload

---
+++ datatype.dart
@@ -0,0 +1,22 @@
+void main(){
+  //int
+  int marks = 91;
+  var count = 53;
+
+  //double
+  double percentage = 93.40;
+  var percent = 93.32;
+  double exponent = 1.42e5;
+
+  //strings
+  String name = "lily";
+  var brand ="kraus";
+
+  //boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(marks);
+  print(exponent);
+  print(brand);
+}
